90f80a56dd22a68896d3a74eb7d255912cf7d25d
rmoncrief <rmoncri1@vols.utk.edu>
2016-08-03 08:52:58 -0400

    Finished routing with components

---
+++ PluralSight/ps-movies/movie-details.component.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+  var module = angular.module("psMovies");
+
+  module.component("movieDetails", {
+    templateUrl: "/ps-movies/movie-details.component.html",
+    controllerAs: "model",
+    controller: function  () {
+      var model = this;
+
+      model.$routerOnActivate = function(next) { 
+        model.id = next.params.id;
+      };
+    }
+  });
+
+}());
